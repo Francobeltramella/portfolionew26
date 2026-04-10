fe39f959b9ee8f19687aeeb5e406dae9bc932579
Franco <franbeltramella@hotmail.com>
2026-04-10 13:58:24 -0300

    first push

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
+import fs from "fs";
+
+const files = fs.readdirSync("./src");
+
+const input = Object.fromEntries(
+  files
+    .filter(f => f.endsWith(".js"))
+    .map(f => [f.replace(".js", ""), `./src/${f}`])
+);
+
 export default {
-    base: './',
-    build: {
-      outDir: 'dist',
-      assetsDir: './',
-    },
-  };
-  
-  
+  build: {
+    rollupOptions: {
+      input,
+      output: {
+        entryFileNames: "[name].js"
+      }
+    }
+  }
+};
