1da9440a067e2b8535bd89003079f1b4dde0a60f
Franco <franbeltramella@hotmail.com>
2026-04-13 15:49:33 -0300

    first push

--- src/portfolio.js
+++ src/portfolio.js
@@ -16,7 +16,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(0, 3, 21);
+camera.position.set(0, 3, 25);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
